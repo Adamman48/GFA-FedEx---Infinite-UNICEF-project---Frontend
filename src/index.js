24f6433690580d73862f2b8124ece8ce3ea4612c
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-05-03 10:34:55 +0200

    Merged master.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import store from './store/store';
 import QuestionContainer from './containers/question-container';
 import WelcomePageContainer from './containers/welcome-page-container';
 
